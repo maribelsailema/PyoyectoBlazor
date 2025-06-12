cb9927129d2843d8bbbb02f562c5f3d43f097e8d
Heidi <hvillavicencio8210@uta.edu.ec>
2025-06-12 00:15:44 -0500

    Agregar clase en Shared

---
+++ Proyecto.Fonted/Services/AuthService.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using static Proyecto.Fonted.Pages.Registro;
+
+namespace Proyecto.Fonted.Services
+{
+    public class AuthService
+    {
+        private readonly HttpClient _http;
+
+        public AuthService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<bool> Register(RegisterModel model)
+        {
+            var response = await _http.PostAsJsonAsync("api/auth/register", model);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ Proyecto.Shared/Models/RegisterModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto.Shared.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
+
