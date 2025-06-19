155daefe346dfef332f2e1519111e051b0bd7778
Heidi <hvillavicencio8210@uta.edu.ec>
2025-06-18 23:49:11 -0500

    Importar Obras

---
+++ Proyecto.Fonted/Services/InvestigacionService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using Proyecto.Shared.Models; // Cambia si tu modelo está en otro namespace
+
+namespace Proyecto.Frontend.Services
+{
+    public class InvestigacionService
+    {
+        private readonly HttpClient _http;
+        private object http;
+
+        public InvestigacionService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<Investigacion>> ObtenerInvestigacionesAsync()
+        {
+            return await _http.GetFromJsonAsync<List<Investigacion>>("api/Investigaciones/Listar");
+
+        }
+        public async Task<List<Investigacion>> ObtenerPorCedulaAsync(string cedula)
+        {
+            return await _http.GetFromJsonAsync<List<Investigacion>>($"api/Investigaciones/PorCedula/{cedula}");
+        }
+
+
+    }
+}
