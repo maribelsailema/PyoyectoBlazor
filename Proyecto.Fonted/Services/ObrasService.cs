36b08f23ed8cec75db05ff3c98cf9ece92d27dae
SebasIsd <sdsnt2003@gmail.com>
2025-06-26 13:43:10 -0500

    cambios de obras

---
+++ Proyecto.Fonted/Services/ObrasService.cs
@@ -0,0 +1,25 @@
+using System.Net.Http.Json;
+using Proyecto.Shared.Models;
+
+namespace Proyecto.Frontend.Services
+{
+    public class ObrasService
+    {
+        private readonly HttpClient _http;
+
+        public ObrasService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<Obra>> ObtenerPorCedulaAsync(string cedula)
+        {
+            var resultado = await _http.GetFromJsonAsync<List<Obra>>($"api/capacitaciones/cedula/{cedula}");
+            return resultado ?? new List<Obra>();
+        }
+
+
+        
+
+    }
+}
