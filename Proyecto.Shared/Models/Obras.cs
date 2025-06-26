636f226497a0236e0ebd2ee75922a47fdb490338
SebasIsd <sdsnt2003@gmail.com>
2025-06-26 13:48:46 -0500

    Delete Obras.cs

--- Proyecto.Shared/Models/Obras.cs
+++
@@ -1,8 +0,0 @@
-public class Obra
-{
-    public int IdObra { get; set; }
-    public string Cedula { get; set; } = string.Empty;
-    public string TipoObra { get; set; } = string.Empty;
-    public DateOnly Fecha { get; set; }
-    public byte[]? Pdf { get; set; }
-}
