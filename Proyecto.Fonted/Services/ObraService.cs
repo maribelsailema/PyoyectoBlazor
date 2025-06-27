a71ce951f94bfeaace060dc59b02d2a50216c490
SebasIsd <sdsnt2003@gmail.com>
2025-06-27 04:01:30 -0500

    mas commits

---
+++ Proyecto.Fonted/Services/ObraService.cs
@@ -0,0 +1,25 @@
+﻿using Proyecto.Backend.Domain.Entities.Models;
+using System.Net.Http.Json;
+using Proyecto.Shared.Models;
+
+public class ObraService
+{
+    private readonly HttpClient _http;
+    public ObraService(HttpClient http) => _http = http;
+
+    public async Task<List<Obra>> ObtenerPorCedulaAsync(string cedula)
+    {
+        var resp = await _http.GetAsync($"api/Obras/buscar/{cedula}");
+        return resp.IsSuccessStatusCode
+            ? await resp.Content.ReadFromJsonAsync<List<Obra>>() ?? new List<Obra>()
+            : new List<Obra>();
+    }
+
+    public async Task<Obra?> GuardarObraAsync(Obra obra)
+    {
+        var resp = await _http.PostAsJsonAsync("api/Obras", obra);
+        return resp.IsSuccessStatusCode
+            ? await resp.Content.ReadFromJsonAsync<Obra>()
+            : null;
+    }
+}
---
+++ Proyecto.Shared/Models/Obra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Proyecto.Backend.Domain.Entities.Models;
+
+public partial class Obra
+{
+    public int IdObra { get; set; }
+
+    public string Cedula { get; set; } = null!;
+
+    public string TipoObra { get; set; } = null!;
+
+    public DateOnly Fecha { get; set; }
+
+    public byte[]? Pdf { get; set; }
+}
