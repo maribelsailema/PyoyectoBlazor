321a06986fbf9f1131c513029afce26031b17096
Ari <arielcholotabas@gmail.com>
2025-07-02 14:21:25 -0500

    Probando enviar cambios

---
+++ Proyecto.Shared/Models/EvaluacionesDocente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Shared.Models
+{
+    internal class EvaluacionesDocente
+    {
+        public int IdEval { get; set; }
+
+        public string Cedula { get; set; } = null!;
+
+        public string Periodo { get; set; } = null!;
+
+        public decimal PuntajeFinal { get; set; }
+
+        public DateOnly FechaEvaluacion { get; set; }
+
+        public byte[]? Pdf { get; set; }  // PDF en binario (VARBINARY)
+
+        public virtual Usuario CedulaNavigation { get; set; } = null!;
+    }
+}
